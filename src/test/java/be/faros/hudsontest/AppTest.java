3a810ad516082d3114577ac7aa9e2283de03f4f0
Bart Vaes <bart.vaes@faros.be>
2012-10-09 13:14:10 +0200

    Fix test !

--- src/test/java/be/faros/hudsontest/AppTest.java
+++ src/test/java/be/faros/hudsontest/AppTest.java
@@ -16,7 +16,6 @@ public class AppTest extends TestCase
      */
     public AppTest( String testName )
     {
-	  breakage !
         super( testName );
     }
 
@@ -33,6 +32,6 @@ public class AppTest extends TestCase
      */
     public void testApp()
     {
-        assertTrue( true );
+        assertTrue( false );
     }
 }
